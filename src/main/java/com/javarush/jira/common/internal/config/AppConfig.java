8811ce89983032a7ae4fe602bfc280bcd2ce9488
unknown <enzzomatrix@gmail.com>
2024-11-12 11:46:48 +1100

    upd

--- src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
+++ src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
@@ -8,15 +8,21 @@ import com.javarush.jira.common.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.http.ProblemDetail;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.sql.DataSource;
 import java.util.Map;
 import java.util.concurrent.Executor;
 
@@ -42,6 +48,28 @@ public class AppConfig {
                 setThreadNamePrefix("mail-");
             }
         };
+    }
+
+    @Bean
+    @Profile("!test")
+    public DataSource postgresDataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+    @Bean
+    @Profile("test")
+    public DataSource h2DataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:/db/schema-h2.sql")
+                .addScript("classpath:/db/data-h2.sql")
+                .build();
     }
 
     public boolean isProd() {
